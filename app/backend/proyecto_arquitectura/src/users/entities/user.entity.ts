1fa452412cdcf3d49de90e06fdd1dd2d04ccfcdf
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-10-08 13:54:14 -0600

    Agregar todos los archivos del backend al repositorio principal

---
+++ app/backend/proyecto_arquitectura/src/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import type { UUID } from "crypto";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: UUID;
+
+    @Column({ type: "varchar", length: 255 , unique: true, nullable: false })
+    email: string;
+
+    @Column({ type: "varchar", length: 255, nullable: false })
+    password: string;
+
+    @Column({ type: "varchar", length: 255, nullable: false })
+    name: string;
+
+    @Column({ type: "varchar", length: 255, nullable: false, default: "user" })
+    rol: string;
+
+    @Column({ type: "boolean", default: true })
+    isActive: boolean;
+}
